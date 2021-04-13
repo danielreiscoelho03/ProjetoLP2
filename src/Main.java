8def390b82823ce9966684a9e43ed16ccc112f1f
Fabio Mesquita <fabiomesquita1904@gmail.com>
2021-04-13 10:23:02 +0100

    teste

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println(p.toString());
         System.out.println("O meu puro " + p.getName() + " find the treasure!!");
         System.out.println("END OF THE GAME!!");
+        System.out.println("ola");
     }
 }
