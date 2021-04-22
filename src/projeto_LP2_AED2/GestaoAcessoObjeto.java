a5513d8518de04eeea47c78722f12f6f7165bf8f
DESKTOP-FRB1AOC\Daniel <39815@ufp.edu.pt>
2021-04-22 18:07:25 +0100

    erros again

--- src/projeto_LP2_AED2/GestaoAcessoObjeto.java
+++ src/projeto_LP2_AED2/GestaoAcessoObjeto.java
@@ -5,12 +5,12 @@ import Search.BST_AED2_2021;
 public class GestaoAcessoObjeto implements GestaoObjetos{
     private int numObjeto = 1;
     public BST_AED2_2021<Integer, Cache> objetos = new BST_AED2_2021<>();
-    /**
+
     @Override
     public boolean regista(Objeto objeto) {
         //objetos.put(numObjeto,objeto);
+        return false;
     }
-     **/
 
     @Override
     public boolean remove(Integer idObjeto) {
--- src/projeto_LP2_AED2/GestaoObjetos.java
+++ src/projeto_LP2_AED2/GestaoObjetos.java
@@ -2,7 +2,7 @@ package projeto_LP2_AED2;
 
 public interface GestaoObjetos {
 
-  public boolean regista(String nome);
+  public boolean regista(Objeto objeto);
   public boolean remove(Integer idObjeto);
   public boolean existe(Integer idObjeto);
 
